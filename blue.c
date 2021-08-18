521ae0ac1640f7050d312d4744ea8c03a387c319
Khrob Edmonds <khedmonds@gmail.com>
2021-08-18 14:31:14 +0800

    do something with the mouse input

--- blue.c
+++ blue.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void update (float t);
+
+typedef struct Input
+{
+	float x,y;
+	bool  mouse_down;
+} Input;
+
+void update (float t, void *input);
 void render ();
 void (*push_rect) (float, float, float, float, float, float, float, float, uint16_t);
 void (*open_window) ();
@@ -25,17 +32,34 @@ int main ()
 	return 0;
 }
 
-void update (float t)
+float sx,sy,ex,ey;
+bool just_down = false;
+
+void update (float t, void *input)
 {
-	// printf("elapsed: %f\n", t);
+	Input *i = ((Input *)input);
+
+	if (!i->mouse_down) { just_down = false; }
+
+	if (i->mouse_down && !just_down) {
+		just_down = true;
+		sx = i->x;
+		sy = i->y;
+		printf("c: %f,%f\n", i->x, i->y);
+	}
+
+	if (just_down && i->mouse_down) {
+		ex = i->x;
+		ey = i->y;
+	}
 }
 
 void render ()
 {
-	// printf("render called\n");
+	// push_rect(0.1,0.1, 0.2,0.2, 1.0,0.0,0.0,1.0, 0);
+	// push_rect(0.0,0.0, 0.1,0.1, 0.0,1.0,0.0,1.0, 0);
+	// push_rect(0.9,0.5, 0.1,0.5, 1.0,0.0,1.0,1.0, 0);
 
-	push_rect(0.1,0.1, 0.2,0.2, 1.0,0.0,0.0,1.0, 0);
-	push_rect(0.0,0.0, 0.1,0.1, 0.0,1.0,0.0,1.0, 0);
-	push_rect(0.9,0.5, 0.1,0.5, 1.0,0.0,1.0,1.0, 0);
+	push_rect(sx,sy, ex-sx, ey-sy, 0,0,1,1, 0);
 }
 
